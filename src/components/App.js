c08b700b0bc149a53302a78ef86408b68e3d1861
himoura32 <shawn.ahearn@outlook.com>
2022-06-02 09:57:40 -0400

    coded actionCreator/components/axiosGET

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import PostList from './PostList';
 
 function App() {
   return (
     <div className='ui container'>
-      <h1>App</h1>
+      <h1><PostList /></h1>
     </div>
   );
 }
